76137676a5519ee15c8605e97862b24655ac4aaf
briecodes <hello@brie.work>
2018-05-04 10:22:57 -0400

    most functionality built.

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,35 @@
 class SuperheroesController < ApplicationController
+  before_action :get_hero, only: [:show]
+  
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+  end
+  
+  def create
+    @superhero = Superhero.create(superhero_params)
+    if @superhero.valid?
+      redirect_to @superhero
+    else
+      flash[:errors] = @superhero.errors.full_messages
+      
+      redirect_to new_superhero_path
+    end
+  end
+
+  private
+    def get_hero
+      @superhero = Superhero.find(params[:id])
+    end
+
+    def superhero_params
+      params.require(:superhero).permit(:name, :super_name, :superpower_id)
+    end
 end
--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,5 +1,15 @@
 class SuperpowersController < ApplicationController
+  before_action :get_power, only: [:show]
+
   def index
     @superpowers = Superpower.all
   end
+
+  def show
+  end
+
+  private
+    def get_power
+      @superpower = Superpower.find(params[:id])
+    end
 end
